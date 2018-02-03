b12de11e73652057db9a70c962a84abe9de3d41d
David Gao <David@Davids-MacBook-Air.local>
2018-02-03 01:00:20 -0500

    Added graphics library

--- app/src/main/java/rdm/qhacks/com/musicmatch/MusicMatch.java
+++ app/src/main/java/rdm/qhacks/com/musicmatch/MusicMatch.java
@@ -3,11 +3,16 @@ package rdm.qhacks.com.musicmatch;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class MusicMatch extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_music_match);
+
+
     }
 }
