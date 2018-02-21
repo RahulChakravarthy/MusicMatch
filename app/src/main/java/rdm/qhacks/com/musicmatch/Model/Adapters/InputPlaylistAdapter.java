7ae4a57d34db2056de32d998382ffe0b6bd78fff
Rahul-Chakravarthy <rahul.chakravarthy101@gmail.com>
2018-02-21 00:01:26 -0500

    continued working on adapters

--- app/src/main/java/rdm/qhacks/com/musicmatch/Model/Adapters/InputPlaylistAdapter.java
+++ app/src/main/java/rdm/qhacks/com/musicmatch/Model/Adapters/InputPlaylistAdapter.java
@@ -1,37 +1,68 @@
 package rdm.qhacks.com.musicmatch.Model.Adapters;
 
 
+import android.annotation.SuppressLint;
 import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
 
-public class InputPlaylistAdapter extends RecyclerView.Adapter<InputPlaylistAdapter.InputSongDOViewHolder>{
+import rdm.qhacks.com.musicmatch.Model.DataObject.InputPlaylistDO;
+import rdm.qhacks.com.musicmatch.Model.DataObject.SongDO;
+import rdm.qhacks.com.musicmatch.R;
+
+public class InputPlaylistAdapter extends RecyclerView.Adapter<InputPlaylistAdapter.InputSongDOViewHolder> {
+
+    private InputPlaylistDO inputPlaylistDO = new InputPlaylistDO();
 
     // Nested class to initialize the views of rows
     static class InputSongDOViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
 
+        TextView songName;
+        TextView fileSize;
+        ImageView removeButton;
+
         public InputSongDOViewHolder(View itemView) {
             super(itemView);
             itemView.setOnClickListener(this);
+
         }
         @Override
         public void onClick(View view) {
+            //do nothing
+        }
 
+        @SuppressLint("DefaultLocale")
+        public void bindData(SongDO songDO){
+            this.songName.setText(songDO.getSongName());
+            this.fileSize.setText(String.format("%d", songDO.getFileSize()));
         }
     }
 
+
     @Override
     public InputSongDOViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
-        return null;
+        View inflatedView = LayoutInflater.from(parent.getContext()).inflate(R.layout.input_playlist, parent, false);
+        return new InputSongDOViewHolder(inflatedView);
     }
 
     @Override
     public void onBindViewHolder(InputSongDOViewHolder holder, int position) {
-
+        SongDO songDO = inputPlaylistDO.getSetOfSongs().get(position);
+        holder.bindData(songDO);
     }
 
     @Override
     public int getItemCount() {
-        return 0;
+        return this.inputPlaylistDO.getSetOfSongs().size();
+    }
+
+    /**
+     * Updates the current selection of SongDO's with a new one
+     */
+    public void swapData(){
+
     }
 }
--- app/src/main/java/rdm/qhacks/com/musicmatch/Model/DataObject/SongDO.java
+++ app/src/main/java/rdm/qhacks/com/musicmatch/Model/DataObject/SongDO.java
@@ -9,9 +9,36 @@ public class SongDO extends DataObject {
     private File songFile;
     private String filePath;
     private String songName;
+    private Long fileSize;
 
     public SongDO(String filePath) {
         this.filePath = filePath;
         this.songFile = new File(filePath);
+        this.songName = this.songFile.getName();
+        this.fileSize = this.songFile.length();
+    }
+
+    public String getFilePath() {
+        return filePath;
+    }
+
+    public void setFilePath(String filePath) {
+        this.filePath = filePath;
+    }
+
+    public String getSongName() {
+        return songName;
+    }
+
+    public void setSongName(String songName) {
+        this.songName = songName;
+    }
+
+    public Long getFileSize() {
+        return fileSize;
+    }
+
+    public void setFileSize(Long fileSize) {
+        this.fileSize = fileSize;
     }
 }
