96dde02bde21dc02e447ec8ae2b0c153fc3cf11f
Rahul Chakravarthy <rahul.chakravarthy101@gmail.com>
2018-04-04 18:54:22 -0400

    Started processing music

--- app/src/main/java/rdm/qhacks/com/musicmatch/Controllers/MusicGController.java
+++ app/src/main/java/rdm/qhacks/com/musicmatch/Controllers/MusicGController.java
@@ -3,10 +3,16 @@ package rdm.qhacks.com.musicmatch.Controllers;
 
 import com.musicg.wave.Wave;
 import com.musicg.wave.WaveFileManager;
+import com.musicg.wave.WaveHeader;
+
+import org.json.JSONException;
+import org.json.JSONObject;
 
 import java.io.File;
+import java.util.Arrays;
 
 import rdm.qhacks.com.musicmatch.Model.DataObject.Music.MusicModel.BaseMusicModel;
+import rdm.qhacks.com.musicmatch.Model.DataObject.Music.MusicModel.MusicModelDO;
 
 /**
  * Main controller for initialization and setup of MusicG api
@@ -16,11 +22,42 @@ public class MusicGController extends BaseController {
     public MusicGController(){
     }
 
-    public BaseMusicModel processSong(File file){
-        Wave wave = new Wave(file.getAbsolutePath());
-        wave.leftTrim(1);
-        wave.rightTrim(0.5D);
-        return null;
+    public BaseMusicModel processSong(File file) throws JSONException {
+        Wave wave = getFileAsWave(file);
+
+        JSONObject waveHeaderJSON = processWaveHeader(wave.getWaveHeader());
+        JSONObject amplitudesJSON = processAmplitudes(wave.getNormalizedAmplitudes());
+        double[] tempFreq = {1.0d, 2.0d};
+        JSONObject pitchesJSON = processPitches(tempFreq);
+
+        MusicModelDO musicModelDO = new MusicModelDO(10);
+
+        //Add all the additional data
+        musicModelDO.getProcessedContents().put("Wave Header", waveHeaderJSON);
+        musicModelDO.getProcessedContents().put("Amplitudes", amplitudesJSON);
+        musicModelDO.getProcessedContents().put("Pitches", pitchesJSON);
+
+        return musicModelDO;
     }
+
+    private Wave getFileAsWave(File file) {
+        //Do any file conversions here (i.e : MP3 -> WAV)
+        return new Wave(file.getAbsolutePath());
+    }
+
+
+    private JSONObject processWaveHeader(WaveHeader waveHeader) throws JSONException {
+        return new JSONObject("");
+    }
+
+    private JSONObject processAmplitudes(double[] amplitudes) throws JSONException {
+        return new JSONObject("");
+    }
+
+    private JSONObject processPitches(double[] frequencies) throws JSONException {
+        return new JSONObject("");
+    }
+
+
 
 }
--- app/src/main/java/rdm/qhacks/com/musicmatch/Controllers/YoutubeRequestController.java
+++ app/src/main/java/rdm/qhacks/com/musicmatch/Controllers/YoutubeRequestController.java
@@ -1,5 +1,8 @@
 package rdm.qhacks.com.musicmatch.Controllers;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * This controller handles all requests made to Youtube's data service api in order to request audio songs
  */
@@ -8,5 +11,16 @@ public class YoutubeRequestController extends BaseController {
     public YoutubeRequestController(){
 
     }
+
+    /**
+     * Takes in the appropriate query parameters and makes a youtube search to find similar songs,
+     * For copyright reasons we do not download the song, but rather send as much meta data back to the user's phone
+     * so they can research it themselves
+     * @return
+     */
+    public List<String> fetchSimilarMusic(){
+        return new ArrayList<>();
+    }
+
 
 }
